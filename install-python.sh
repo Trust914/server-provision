52d90f06edaed1baaa2f45db4816092b0c290bd3
Trust Ezekiel-Okezie <Trustezekiel.okezie@gmail.com>
2023-07-04 02:16:01 +0100

    Added provisioner

---
+++ install-python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##Ansible requires that remote hosts have python installed in them
+
+yum update -y
+# yum install wget unzip httpd -y
+# systemctl start httpd
+# systemctl enable httpd
+# cd /tmp/
+# wget https://www.tooplate.com/zip-templates/2130_waso_strategy.zip
+# unzip -o 2130_waso_strategy.zip
+# cp -r 2130_waso_strategy/* /var/www/html/
+# systemctl restart httpd
+yum install python3 -y
